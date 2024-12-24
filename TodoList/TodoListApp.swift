9960addbf09b25e67f4d4aacfd78530ba4beb4d9
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2024-12-23 19:55:51 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Caleb Cupul on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
